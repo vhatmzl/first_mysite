869ee07da44b4fe150455aed8ec8e2509e6b90c3
Your Name <you@example.com>
2024-03-29 15:09:57 +0900

    notebook

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Question, Choice# .은 현재 있는 admin.py에 있는 models를 가져온다는 얘기
 # Register your models here.
+
+admin.site.register(Question)
+admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+import datetime
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
